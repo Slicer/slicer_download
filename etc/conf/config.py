6a90bbd7911117b36220d3940faacfcc9543e8f8
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-08-12 13:08:40 -0400

    slicer_download_server: Set TEMPLATES_AUTO_RELOAD
    This will ensures the templates downloaded using the script "download-flask-templates-and-assets.sh"
    are reloaded.
    
    The value default to "True" and can be overridden setting the env. variable
    SLICER_DOWNLOAD_TEMPLATES_AUTO_RELOAD.
    
    See https://flask.palletsprojects.com/en/2.0.x/config/#TEMPLATES_AUTO_RELOAD

--- etc/conf/config.py
+++ etc/conf/config.py
@@ -7,3 +7,4 @@ from config_utils import toBool  # noqa: E402
 
 DB_FALLBACK = toBool(os.environ.get("SLICER_DOWNLOAD_DB_FALLBACK", False))
 DEBUG = toBool(os.environ.get("SLICER_DOWNLOAD_DEBUG", False))
+TEMPLATES_AUTO_RELOAD = toBool(os.environ.get("SLICER_DOWNLOAD_TEMPLATES_AUTO_RELOAD", True))
