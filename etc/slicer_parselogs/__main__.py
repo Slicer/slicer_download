9cac2a08e547680c38ec05156ff8517322593251
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-10-19 14:06:21 -0400

    slicer_parselogs: Simplify database connection handling using context manager

--- etc/slicer_parselogs/__main__.py
+++ etc/slicer_parselogs/__main__.py
@@ -29,30 +29,28 @@ def main():
     filenames = args.filenames
     statsdata = args.statsdata
 
-    db = openDb(dbname)
+    with openDb(dbname) as db:
 
-    access.create_access_table(db)
-    bitstream.create_bitstream_table(db)
-    geoip.create_geoip_table(db)
-    useragent.create_useragent_table(db)
+        access.create_access_table(db)
+        bitstream.create_bitstream_table(db)
+        geoip.create_geoip_table(db)
+        useragent.create_useragent_table(db)
 
-    # parse apache logs, if they exist, and add them to db
-    access.add_access_info(db, filenames)
+        # parse apache logs, if they exist, and add them to db
+        access.add_access_info(db, filenames)
 
-    # each of these items depends on the access table
-    geoip.add_geoip_info(db, geoip_filename)
-    useragent.add_useragent_info(db)
-    if not args.nomidas:
-        bitstream.add_bitstream_info(db, getRecordsFromURL())
+        # each of these items depends on the access table
+        geoip.add_geoip_info(db, geoip_filename)
+        useragent.add_useragent_info(db)
+        if not args.nomidas:
+            bitstream.add_bitstream_info(db, getRecordsFromURL())
 
-    # then write out slicer json
+        # then write out slicer json
+        slicer_stats_data = slicerstats.get_download_stats_data(db)
+        with open(statsdata, 'w+') as statsfp:
+            print('writing %s' % statsdata)
+            json.dump(slicer_stats_data, statsfp, separators=(',', ':'))
 
-    slicer_stats_data = slicerstats.get_download_stats_data(db)
-    with open(statsdata, 'w+') as statsfp:
-        print('writing %s' % statsdata)
-        json.dump(slicer_stats_data, statsfp, separators=(',', ':'))
-
-    db.close()
     sys.exit(0)
 
 
