Slicer/slicer_download

commit 8d53024541ff77093b75084059f45c5c022c9f65

slicer_getbuildinfo: Add support for removing items

etc/slicer_getbuildinfo/__main__.py
@@ -102,6 +102,7 @@ def displayDuplicateDrafts(duplicates):
 def main():
     argparser = argparse.ArgumentParser(description="Download Slicer application package metadata and update sqlite database")
     argparser.add_argument("--display-duplicate-drafts", action="store_true", help="Display duplicate draft folders & items and exit")
+    argparser.add_argument("--remove-itemids", help="comma separated list of itemid to remove from the database")
     argparser.add_argument("dbfile", metavar="DB_FILE", nargs="?")
     args = argparser.parse_args()
     dbfile = args.dbfile
@@ -113,6 +114,13 @@ def main():
         duplicates = collectDuplicates(records)
         displayDuplicateDrafts(duplicates)
         sys.exit(0)
+
+    itemIdsToRemove = set()
+    if args.remove_itemids:
+        for itemId in args.remove_itemids.split(","):
+            if len(itemId) != 24:
+                argparser.error("ID of item to eclude is expected to be 24 characters")
+            itemIdsToRemove.add(itemId)
 
     if dbfile is None:
         argparser.error("No action requested, specify --display-duplicate-drafts or DB_FILE")
@@ -151,6 +159,21 @@ def main():
 
     print("Saved {0}".format(dbfile))
 
+    if len(itemIdsToRemove) > 0:
+        print("")
+        duplicatesByItemId = {}
+        for key, ids in collectDuplicates(records).items():
+            for itemId, _ in ids:
+                duplicatesByItemId[itemId] = key
+
+        with sqlite3.connect(dbfile) as db:
+            print(f"Removed {len(itemIdsToRemove)} rows")
+            for itemId in itemIdsToRemove:
+                db.execute("delete from _ where item_id=?", (itemId, ))
+                print(f"  {itemId} ({duplicatesByItemId[itemId]})")
+            db.commit()
+
+        print("Saved {0}".format(dbfile))
 
 if __name__ == '__main__':
     main()

bin/cron-getbuildinfo.sh
@@ -43,4 +43,4 @@ echo "[slicer_getbuildinfo] Using these directories"
 echo "  ROOT_DIR       : ${ROOT_DIR}"
 
 echo
-PYTHONPATH=${ROOT_DIR} "${PYTHON_EXECUTABLE}" "${ROOT_DIR}/etc/slicer_getbuildinfo" ${SLICER_DOWNLOAD_DB_FILE}
+PYTHONPATH=${ROOT_DIR} "${PYTHON_EXECUTABLE}" "${ROOT_DIR}/etc/slicer_getbuildinfo" $* ${SLICER_DOWNLOAD_DB_FILE}

commit 7ee8e34a9f13852275177f782e61541e307d0ba8

slicer_getbuildinfo: Fix removal of itemId by querying local db
This commit fixes a regression introduced in 8d53024 (Add support for
removing items). It ensures the lookup of package key associated with
the itemId being removed is successful by querying the local database
instead of assuming the record still exist on the server.

etc/slicer_getbuildinfo/__main__.py
@@ -12,6 +12,13 @@ from slicer_download import (
 )
 
 
+def getRecordsFromDB(dbfile):
+    with sqlite3.connect(dbfile) as db:
+        cursor = db.cursor()
+        cursor.execute("select record from _")
+        return [json.loads(row[0]) for row in cursor.fetchall()]
+
+
 def midasRecordToDb(r):
     try:
         return [int(r['item_id']),
@@ -164,16 +171,17 @@ def main():
 
     if len(itemIdsToRemove) > 0:
         print("")
-        duplicatesByItemId = {}
-        for key, ids in collectDuplicates(records).items():
+        packages = applicationPackageToIDs(getRecordsFromDB(dbfile))
+        packagesByItemId = {}
+        for key, ids in packages.items():
             for itemId, _ in ids:
-                duplicatesByItemId[itemId] = key
+                packagesByItemId[itemId] = key
 
         with sqlite3.connect(dbfile) as db:
             print(f"Removed {len(itemIdsToRemove)} rows")
             for itemId in itemIdsToRemove:
                 db.execute("delete from _ where item_id=?", (itemId, ))
-                print(f"  {itemId} ({duplicatesByItemId[itemId]})")
+                print(f"  {itemId} ({packagesByItemId[itemId]})")
             db.commit()
 
         print("Saved {0}".format(dbfile))